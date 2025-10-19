15251b44d9215af76724e7b4dca2b123a3bb7c39
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2025-10-19 14:13:27 +0200

    WIP

---
+++ ProtegeMCP.Server/Requests/Axiom/ListConceptAxiomsRequest.cs
@@ -0,0 +1,3 @@
+namespace ProtegeMCP.Server.Requests.Axiom;
+
+public record ListConceptAxiomsRequest(string Uri);
---
+++ ProtegeMCP.Server/Tools/AxiomTools.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel;
+using ModelContextProtocol.Server;
+
+namespace ProtegeMCP.Server.Tools;
+
+
+[McpServerToolType, Description("Tools for CRUD operations on Axioms of Concepts")]
+public class AxiomTools
+{
+    [McpServerTool(Name = "list-axioms")]
+    [Description("List all axioms organized by category for given Concept in current Ontology")]
+    public static async Task<string> ListConceptsAsync(HttpClient client)
+    {
+        var response = await client.GetAsync("/list-concept-axioms");
+        return await response.Content.ReadAsStringAsync();
+    }
+}
